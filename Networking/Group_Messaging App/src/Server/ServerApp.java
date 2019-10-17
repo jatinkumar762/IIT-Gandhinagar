3c3e8040ba9a90421af51a61704fd6b41a63fecb
Jatin Kumar <jatin.kumar762@gmail.com>
2019-10-17 18:15:48 +0530

    Create ServerApp.java

---
+++ Networking/Group_Messaging App/src/Server/ServerApp.java
@@ -0,0 +1,14 @@
+package Server;
+
+public class ServerApp {
+
+	public ServerApp() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
