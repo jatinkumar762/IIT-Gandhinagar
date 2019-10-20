a20d0c88db43e934acad65f7e107589edd0ff424
Jatin Kumar <jatin.kumar762@gmail.com>
2019-10-21 03:33:29 +0530

    Update LoginWin.java

--- Networking/Group_Messaging App/src/Client/LoginWin.java
+++ Networking/Group_Messaging App/src/Client/LoginWin.java
@@ -2,14 +2,18 @@ package Client;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPasswordField;
 import javax.swing.JRadioButton;
 import javax.swing.JTextField;
+
 
 public class LoginWin extends JFrame implements ActionListener {
 
@@ -23,6 +27,7 @@ public class LoginWin extends JFrame implements ActionListener {
 		// TODO Auto-generated constructor stub
 		   this.setLayout(null);
 		   this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
+		   
 		if(type=="signin") {
 			
 	       idlbl=new JLabel("User Name:");
@@ -37,14 +42,13 @@ public class LoginWin extends JFrame implements ActionListener {
 	       this.add(pwdlbl);
 	       this.add(pwd);
 	       this.add(logbtn);
-	       this.add(SignUpbtn);
 	       
 	       idlbl.setBounds(40,110,100,25);
 	       logid.setBounds(120,110,150,25);
 	       pwdlbl.setBounds(40,150,100,25);
 	       pwd.setBounds(120,150,150,25);
 	       logbtn.setBounds(100,190,100,25);
-	       SignUpbtn.setBounds(100,190,100,25);
+	      
 	       
 		}else {
 			
@@ -55,10 +59,10 @@ public class LoginWin extends JFrame implements ActionListener {
 		       logid=new JTextField();
 		       pwd=new JPasswordField();
                cnfpwd=new JPasswordField();
-		       
-		       SignUpbtn=new JButton("Sign Up");
-		       SignUpbtn.addActionListener(this);
-		       
+		      
+               SignUpbtn=new JButton("Sign Up");
+               SignUpbtn.addActionListener(this);
+    	       
 		       this.add(idlbl);
 		       this.add(logid);
 		       this.add(pwdlbl);
@@ -81,6 +85,41 @@ public class LoginWin extends JFrame implements ActionListener {
 
 	public void actionPerformed(ActionEvent ae)
     {
-	 
+		try {
+			
+		 ObjectOutputStream out=new ObjectOutputStream(ClientRes.client.getOutputStream());     
+         ObjectInputStream in=new ObjectInputStream(ClientRes.client.getInputStream());
+         
+	      if(ae.getSource().equals(SignUpbtn)) {
+	    	  
+	         out.writeObject("NewUser");
+	         out.writeObject(this.logid.getText().trim());
+	         out.writeObject(new String(this.pwd.getPassword()).trim());	                  
+	         String resp=in.readObject().toString();
+	         
+	         JOptionPane.showMessageDialog(this,resp,"User Registration",JOptionPane.INFORMATION_MESSAGE);  
+	         ClientRes.logid = this.logid.toString();
+	        
+	        		 
+	      }else {    	
+	    	 out.writeObject("LoginD");
+	         out.writeObject(this.logid.getText().trim());
+	         out.writeObject(new String(this.pwd.getPassword()).trim());      
+	         String resp=in.readObject().toString();         
+	         ClientRes.logid = this.logid.toString();
+	         
+	         
+	         
+	         
+	         
+	      }
+	      
+	      	      
+	      
+	      
+		}
+		catch(Exception ex){
+			
+		}
     }
 }
