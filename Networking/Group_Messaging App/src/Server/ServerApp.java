b5326d56df61e99477b68b3acc24d522aff155e5
Jatin Kumar <jatin.kumar762@gmail.com>
2019-10-20 22:30:27 +0530

    Update ServerApp.java

--- Networking/Group_Messaging App/src/Server/ServerApp.java
+++ Networking/Group_Messaging App/src/Server/ServerApp.java
@@ -1,14 +1,51 @@
 package Server;
 
-public class ServerApp {
+import java.net.*;
 
-	public ServerApp() {
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.io.*;
+
+public class ServerApp extends JFrame {
+
+	public ServerApp() 
+	{
 		// TODO Auto-generated constructor stub
+		try {	
+			    ServerSocket ss=new ServerSocket(6666); 
+			    System.out.println("Server Started.......");
+			 	ServerRes.win=new ServerWin();
+	          	Toolkit tool=Toolkit.getDefaultToolkit();
+	          	Dimension size=tool.getScreenSize();
+	         	final int WIDTH=600;
+	         	final int HEIGHT=600;
+	         	ServerRes.win.setBounds(size.width/2-WIDTH/2,size.height/2-HEIGHT/2,WIDTH,HEIGHT);
+	         	ServerRes.win.setResizable(false);
+	         	ServerRes.win.setTitle("Server Window");
+	         	ServerRes.win.setVisible(true);
+	         	
+			  while(true)
+	          {
+		           System.out.println("Waiting for client...");
+		           Socket client=ss.accept();
+		           System.out.println("Client Connected Successfully");
+		           //invokes thread
+		           new ClientThread(client);
+	          }			  
+		}
+		catch(Exception ex){
+			  JOptionPane.showMessageDialog(this,ex.getMessage(),"Server App",JOptionPane.ERROR_MESSAGE);
+	          return;
+		}	
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
-
+		ServerApp  ServerObj = new ServerApp();	
 	}
 
 }
