64ea8dbb2d1d375b0b6ab579cf85580e3b789124
Jatin Kumar <jatin.kumar762@gmail.com>
2019-10-21 00:45:16 +0530

    Update LoginWin.java

--- Networking/Group_Messaging App/src/Client/LoginWin.java
+++ Networking/Group_Messaging App/src/Client/LoginWin.java
@@ -13,22 +13,23 @@ import javax.swing.JTextField;
 
 public class LoginWin extends JFrame implements ActionListener {
 
-	JLabel idlbl,pwdlbl;
+	JLabel idlbl,pwdlbl,cnfpwdlbl;
     JTextField logid;
-    JPasswordField pwd;
-    JButton logbtn;
-	    
-	public LoginWin() 
+    JPasswordField pwd,cnfpwd;
+    JButton logbtn,SignUpbtn;
+	  
+	public LoginWin(String type) 
 	{
 		// TODO Auto-generated constructor stub
 		   this.setLayout(null);
 		   this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
-		
-	       idlbl=new JLabel("Login ID :");
+		if(type=="signin") {
+			
+	       idlbl=new JLabel("User Name:");
 	       pwdlbl=new JLabel("Password :");
 	       logid=new JTextField();
 	       pwd=new JPasswordField();
-	       logbtn=new JButton("Login");
+	       logbtn=new JButton("Sign In");
 	       logbtn.addActionListener(this);
 
 	       this.add(idlbl);
@@ -36,12 +37,46 @@ public class LoginWin extends JFrame implements ActionListener {
 	       this.add(pwdlbl);
 	       this.add(pwd);
 	       this.add(logbtn);
-		
+	       this.add(SignUpbtn);
+	       
 	       idlbl.setBounds(40,110,100,25);
 	       logid.setBounds(120,110,150,25);
 	       pwdlbl.setBounds(40,150,100,25);
 	       pwd.setBounds(120,150,150,25);
 	       logbtn.setBounds(100,190,100,25);
+	       SignUpbtn.setBounds(100,190,100,25);
+	       
+		}else {
+			
+			   idlbl=new JLabel("Login ID :");
+		       pwdlbl=new JLabel("Password :");
+		       cnfpwdlbl=new JLabel("Re-enter Password :");
+		       
+		       logid=new JTextField();
+		       pwd=new JPasswordField();
+               cnfpwd=new JPasswordField();
+		       
+		       SignUpbtn=new JButton("Sign Up");
+		       SignUpbtn.addActionListener(this);
+		       
+		       this.add(idlbl);
+		       this.add(logid);
+		       this.add(pwdlbl);
+		       this.add(pwd);
+		       this.add(cnfpwdlbl);
+		       this.add(cnfpwd);
+		       this.add(SignUpbtn);
+		       
+		       idlbl.setBounds(40,110,100,25);
+		       logid.setBounds(220,110,150,25);
+		       pwdlbl.setBounds(40,150,100,25);
+		       pwd.setBounds(220,150,150,25);
+		       cnfpwdlbl.setBounds(40,185,200,25);
+		       cnfpwd.setBounds(220,185,150,25);
+		       SignUpbtn.setBounds(100,220,100,25);
+			
+		}
+	       
 	}
 
 	public void actionPerformed(ActionEvent ae)
