57e1b70acb6d08745fb6e8582ffa451ad344ff7f
Jatin Kumar <jatin.kumar762@gmail.com>
2019-10-23 22:09:05 +0530

    Update ClientRes.java

--- Networking/Group_Messaging App/src/Client/ClientRes.java
+++ Networking/Group_Messaging App/src/Client/ClientRes.java
@@ -2,15 +2,22 @@ package Client;
 
 import java.lang.*;
 import java.net.*;
+import java.util.Map;
 import java.io.*;
 
 public class ClientRes
 {
+   public static ClientApp cApp;
+   public static ClientWin cWin;
+   public static LoginWin lWin;
    public static Socket client;
    public static String logid;
+   public static Map<String,String> gpM;
    static
    {
+	   cApp = null;
        client=null;
        logid="";
+       gpM=null;
    }
 }
