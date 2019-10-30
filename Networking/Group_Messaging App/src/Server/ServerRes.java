febbe056a0316ae4c31ec33a8d1491c411a105af
Jatin Kumar <jatin.kumar762@gmail.com>
2019-10-30 23:48:31 +0530

    Update ServerRes.java

--- Networking/Group_Messaging App/src/Server/ServerRes.java
+++ Networking/Group_Messaging App/src/Server/ServerRes.java
@@ -9,12 +9,10 @@ public class ServerRes {
 
 	 public static ServerWin win;
      public static Vector loggedIn;
-     //public static Map<String,Vector> gpMap;
      static
      {
 	   	  win=null;
-	   	  loggedIn=new Vector<Socket>();
-	      //gpMap=new HashMap<String, Vector>();	   	  
+	   	  loggedIn=new Vector<Socket>();	   	  
      }
 
 }
